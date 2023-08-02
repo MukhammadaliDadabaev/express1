8c2a4dd14d79e33448051da55faf56abdd1818b2
Mukhammadali Dadabaev <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2023-08-02 14:16:51 +0500

    created Register, Login done

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-import express from 'express'
-import {
-  create
-} from 'express-handlebars'
-import AuthRouters from './routes/auth.js'
-import ProductsRoutes from './routes/products.js'
+import express from "express";
+import { create } from "express-handlebars";
+import AuthRouters from "./routes/auth.js";
+import ProductsRoutes from "./routes/products.js";
 
-const app = express()
+const app = express();
 const hbs = create({
   defaultLayout: "main",
-  extname: "hbs"
-})
+  extname: "hbs",
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', './views')
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }));
 
-app.use(AuthRouters)
-app.use(ProductsRoutes)
+app.use(AuthRouters);
+app.use(ProductsRoutes);
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: "Best Shop | Ali"
-  })
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Best Shop | Ali",
+  });
+});
 
-app.get('/main', (req, res) => {
-  res.render('main', {
+app.get("/main", (req, res) => {
+  res.render("main", {
     title: "Main | Ali",
-    isMain: true
-  })
-})
+    isMain: true,
+  });
+});
 
-app.get('/add', (req, res) => {
-  res.render('add', {
+app.get("/add", (req, res) => {
+  res.render("add", {
     title: "Add Product | Ali",
-    isAdd: true
-  })
-})
+    isAdd: true,
+  });
+});
 
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT }...!`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...!`));
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,28 @@
-import {
-  Router
-} from "express";
-const router = Router()
+import { Router } from "express";
+const router = Router();
 
-
-router.get('/register', (req, res) => {
-  res.render('register', {
+router.get("/register", (req, res) => {
+  res.render("register", {
     title: "Register | Ali",
-    isRegister: true
-  })
-})
+    isRegister: true,
+  });
+});
 
-router.get('/login', (req, res) => {
-  res.render('login', {
+router.get("/login", (req, res) => {
+  res.render("login", {
     title: "Login | Ali",
-    isLogin: true
-  })
-})
+    isLogin: true,
+  });
+});
 
-export default router
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+export default router;
