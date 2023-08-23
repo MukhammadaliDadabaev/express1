0b116389b0786ffce89494bcd70451caec1881bb
Mukhammadali <ali_6017@mail.ru>
2023-08-23 21:46:53 +0500

    code refactore

--- routes/auth.js
+++ routes/auth.js
@@ -4,21 +4,21 @@ const {
 const User = require("../models/User");
 const router = Router();
 
-router.get("/register", (req, res) => {
+router.get("/register", async (req, res) => {
   res.render("register", {
     title: "Register | Ali",
     isRegister: true,
   });
 });
 
-router.get("/login", (req, res) => {
+router.get("/login", async (req, res) => {
   res.render("login", {
     title: "Login | Ali",
     isLogin: true,
   });
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });
